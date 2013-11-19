0627d377871f67ad898b24f99a60828092e719ff
jbardnz <jbardnz@gmail.com>
2013-11-19 17:07:36 +1300

    First Commit

---
+++ main.js
@@ -0,0 +1,114 @@
+define(function (require, exports, module) {
+    "use strict";
+
+    var CommandManager = brackets.getModule("command/CommandManager");
+    var Menus = brackets.getModule("command/Menus");
+    var EditorManager = brackets.getModule("editor/EditorManager");
+    var right_click_cut = "RightClickExtended.CutHandler"; 
+    var right_click_copy = "RightClickExtended.CopyHandler";
+    var right_click_paste = "RightClickExtended.PasteHandler";
+    var NodeConnection = brackets.getModule("utils/NodeConnection");
+    var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+    var AppInit        = brackets.getModule("utils/AppInit");
+    var nodeConnection = new NodeConnection();
+    
+    function chain() {
+        var functions = Array.prototype.slice.call(arguments, 0);
+        if (functions.length > 0) {
+            var firstFunction = functions.shift();
+            var firstPromise = firstFunction.call();
+            firstPromise.done(function () {
+                chain.apply(null, functions);
+            });
+        }
+    }
+    
+    
+    AppInit.appReady(function () {
+
+        
+        function connect() {
+            var connectionPromise = nodeConnection.connect(true);
+            connectionPromise.fail(function () {
+                console.error("[brackets-simple-node] failed to connect to node");
+            });
+            return connectionPromise;
+        }
+    
+        
+        function loadClipboard() {
+            var path = ExtensionUtils.getModulePath(module, "node/clipboard");
+            var loadPromise = nodeConnection.loadDomains([path], true);
+            loadPromise.fail(function (e) {
+                console.log(e);
+                console.log("[brackets-simple-node] failed to load clipboard");
+            });
+            return loadPromise;
+        }
+        
+        
+        function clipboardLoad() {
+            var loadPromise = nodeConnection.domains.clipboard.load();
+            loadPromise.fail(function (err) {
+                console.error("[brackets-simple-node] failed to run clipboard.load", err);
+            });
+            loadPromise.done(function (err) {
+                //loaded
+
+            });
+            return loadPromise;
+        }
+        
+        chain(connect, loadClipboard, clipboardLoad);
+    });
+                     
+    
+   
+    
+    $(nodeConnection).on("clipboard.paste", function (e, clipboardContent) {
+        var thisEditor = EditorManager.getCurrentFullEditor();
+        var cp = thisEditor._codeMirror.getCursor();
+        thisEditor._codeMirror.replaceSelection(clipboardContent.content);
+        thisEditor._codeMirror.setCursor(cp);
+    });
+    
+    
+    //Function to run when the menu item is clicked
+    function handleRightClickPaste() {
+        
+        nodeConnection.domains.clipboard.callPaste();
+                
+    }
+    
+    function handleRightClickCopy() {
+        
+        var thisEditor = EditorManager.getCurrentFullEditor();
+        
+        nodeConnection.domains.clipboard.callCopy(thisEditor._codeMirror.getSelection());
+        
+    }
+    
+    function handleRightClickCut() {
+        
+        var thisEditor = EditorManager.getCurrentFullEditor();
+        var cp = thisEditor._codeMirror.getCursor();
+        nodeConnection.domains.clipboard.callCopy(thisEditor._codeMirror.getSelection());
+        
+        thisEditor._codeMirror.replaceSelection('');
+        thisEditor._codeMirror.setCursor(cp);
+    }
+    
+    
+    CommandManager.register("Cut", right_click_cut, handleRightClickCut);
+    CommandManager.register("Copy", right_click_copy, handleRightClickCopy);
+    CommandManager.register("Paste", right_click_paste, handleRightClickPaste);
+    
+    Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuDivider();
+    Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuItem(right_click_cut);
+    Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuItem(right_click_copy);
+    Menus.getContextMenu(Menus.ContextMenuIds.EDITOR_MENU).addMenuItem(right_click_paste);
+   
+    
+    
+    //exports.handleRightClickExtended = handleRightClickExtended;
+});
---
+++ node/clipboard.js
@@ -0,0 +1,66 @@
+(function () {
+    "use strict";
+    
+    var node_copy_paste = require('copy-paste');
+    var domainManager;
+    
+    function load(){
+        
+    }
+    
+    
+    function bracketPaste() {
+        
+        node_copy_paste.paste(function(e, content){
+            domainManager.emitEvent('clipboard', 'paste', {'content':content});        
+        });
+
+    }
+    
+    function bracketCopy(userSelection) {
+        
+        node_copy_paste.copy(userSelection);
+
+    }
+    
+
+    function init(DomainManager) {
+        if (!DomainManager.hasDomain("clipboard")) {
+            DomainManager.registerDomain("clipboard", {major: 0, minor: 1});
+        }
+        
+        DomainManager.registerCommand(
+            "clipboard",       // domain name
+            "callPaste",    // command name
+            bracketPaste,   // command handler function
+            false,          // this command is synchronous
+            "Pastes the contents of the users clipboard",
+            [],            
+            [{name: "clipboardContent", type: "string", description: "the contents of the clipboard"}]
+        );
+        DomainManager.registerCommand(
+            "clipboard",       // domain name
+            "callCopy",    // command name
+            bracketCopy,   // command handler function
+            false,          // this command is synchronous
+            "copies current selection",
+            [{name: "userSelection", type: "string", description: "users current selection"}],            
+            []
+        );
+        DomainManager.registerCommand(
+            "clipboard",       // domain name
+            "load",    // command name
+            load,   // command handler function
+            false,          // this command is synchronous
+            "Loads clipboard",
+            [],             // no parameters
+            []
+        );
+        DomainManager.registerEvent('clipboard', 'paste', {name: "content", type: "string", description: "the contents of the clipboard"});
+        
+        domainManager = DomainManager;
+    }
+    
+    exports.init = init;
+    
+}());
