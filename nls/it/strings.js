00471cf85a233523aaeb003f4a463b439ae45e3b
Denisov21 <student@alice.it>
2014-08-19 15:51:09 +0200

    Create strings.js

---
+++ nls/it/strings.js
@@ -0,0 +1,8 @@
+// Italian - strings
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
+define({
+    "COPY"  : "Copia",
+    "CUT"   : "Taglia",
+    "PASTE" : "Incolla",
+});
